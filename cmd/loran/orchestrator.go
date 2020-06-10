0aa61cece5faaf537307fab7d92f376d14e5b33a
Zhaofeng Wan <68039682+CiCiZeo@users.noreply.github.com>
2020-06-10 08:33:00 +0800

    Coingecko fixes

--- cmd/loran/orchestrator.go
+++ cmd/loran/orchestrator.go
@@ -17,6 +17,7 @@ import (
 	"github.com/InjectiveLabs/sdk-go/chain/peggy/types"
 
 	"github.com/InjectiveLabs/loran/orchestrator"
+	"github.com/InjectiveLabs/loran/orchestrator/coingecko"
 	"github.com/InjectiveLabs/loran/orchestrator/cosmos"
 	"github.com/InjectiveLabs/loran/orchestrator/cosmos/tmclient"
 	"github.com/InjectiveLabs/loran/orchestrator/ethereum/committer"
@@ -234,6 +235,7 @@ func orchestratorCmd(cmd *cli.Cmd) {
 		cosmosQueryClient := cosmos.NewPeggyQueryClient(peggyQuerier)
 
 		relayer := relayer.NewPeggyRelayer(cosmosQueryClient, peggyContract, *relayValsets, *relayBatches)
+		coingeckoFeed := coingecko.NewCoingeckoPriceFeed(100, &coingecko.Config{})
 		svc := orchestrator.NewPeggyOrchestrator(
 			cosmosQueryClient,
 			peggyBroadcaster,
@@ -245,6 +247,7 @@ func orchestratorCmd(cmd *cli.Cmd) {
 			parseERC20ContractMapping(*erc20ContractMapping),
 			relayer,
 			*minBatchFeeUSD,
+			coingeckoFeed,
 		)
 
 		ctx, cancelFn := context.WithCancel(context.Background())
--- orchestrator/orchestrator.go
+++ orchestrator/orchestrator.go
@@ -5,6 +5,7 @@ import (
 
 	ethcmn "github.com/ethereum/go-ethereum/common"
 
+	"github.com/InjectiveLabs/loran/orchestrator/coingecko"
 	"github.com/InjectiveLabs/loran/orchestrator/cosmos/tmclient"
 
 	sidechain "github.com/InjectiveLabs/loran/orchestrator/cosmos"
@@ -41,6 +42,7 @@ type peggyOrchestrator struct {
 	erc20ContractMapping map[ethcmn.Address]string
 	relayer              relayer.PeggyRelayer
 	minBatchFeeUSD       float64
+	priceFeeder          *coingecko.CoingeckoPriceFeed
 }
 
 func NewPeggyOrchestrator(
@@ -54,6 +56,8 @@ func NewPeggyOrchestrator(
 	erc20ContractMapping map[ethcmn.Address]string,
 	relayer relayer.PeggyRelayer,
 	minBatchFeeUSD float64,
+	priceFeeder *coingecko.CoingeckoPriceFeed,
+
 ) PeggyOrchestrator {
 	return &peggyOrchestrator{
 		tmClient:             tmClient,
@@ -67,6 +71,7 @@ func NewPeggyOrchestrator(
 		erc20ContractMapping: erc20ContractMapping,
 		relayer:              relayer,
 		minBatchFeeUSD:       minBatchFeeUSD,
+		priceFeeder:          priceFeeder,
 		svcTags: metrics.Tags{
 			"svc": "peggy_orchestrator",
 		},
